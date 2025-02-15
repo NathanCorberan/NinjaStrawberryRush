9182d4b46f2b7b158d2f7fe56f127757c6068017
NathanCorberan <nathan.corberan@supdevinci-edu.fr>
2025-02-15 19:29:57 +0100

    first commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,30 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (Player.transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f)
+        if(Player != null)
         {
-            GameOver();
+            if (Player.transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f)
+            {
+                GameOver();
+            }
         }
+        
     }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
 
+    }
+    public void ExitGame()
+    {
+        // Pour quitter l'application
+        Application.Quit();
+
+        // Si on est en mode éditeur, il ne quittera pas, donc on affiche un message dans la console.
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
     public void GameOver()
     {
         // Vous pouvez charger une scène de game over ou afficher un écran de game over
